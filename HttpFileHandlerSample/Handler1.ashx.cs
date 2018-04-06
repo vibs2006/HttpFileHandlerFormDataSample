9e74b99ff2a63fc60ca1446ccbf2f3e7b75999d5
Vaibhav Agarwal <v.agarwal@ennovate-software.com>
2018-04-06 11:38:05 +0530

    Added Functionality for uploading using Jquery Button without postback

--- HttpFileHandlerSample/Handler1.ashx.cs
+++ HttpFileHandlerSample/Handler1.ashx.cs
@@ -17,31 +17,39 @@ namespace HttpFileHandlerSample
         {
             DeleteAllFiles(context);
 
+            //Simulating delay. Remove Thread.sleep method for production
+            System.Threading.Thread.Sleep(4000); //4 Seconds
+
             List<string> str = new List<string>();
 
             var str1 = new StringBuilder();
             context.Response.ContentType = "text/html";
-            str1.AppendLine("Total Number of files are " + context.Request.Files.Count + "<br />");
-            str1.AppendLine("Total Number of Elemens are " + context.Request.Form.Count + "<br />");
-            
+            str1.AppendLine("<fieldset><legend>Multipart Formdata Statistics</legend>");
+            str1.AppendLine("Total Number of uploaded Files are " + context.Request.Files.Count + "<br />");
+            str1.AppendLine("Total Number of Elements are " + context.Request.Form.Count + "<br />");
+            str1.AppendLine("</fieldset>");
             var files = context.Request.Files;
             var path = context.Server.MapPath("~/upload");
 
             if (!Directory.Exists(path))
             Directory.CreateDirectory(path);
 
-
+            
             for (int i=0;i<=files.Count-1;i++)
             {
                 HttpPostedFile postedFile = files[i];
                 postedFile.SaveAs(path + "\\" + postedFile.FileName);
             }
+            
 
             var Name = context.Request.Form;
+            str1.AppendLine("<fieldset><legend>Name of Input Text Fields with 'name' attribute present</legend>");
+            str1.AppendLine("<b>Attribute Name - Attribute Value</b><br />");
             foreach (var item in Name)
             {
                 str1.AppendLine(item.ToString() + " - " + Name[item.ToString()] + "<br />");
             }
+            str1.AppendLine("</fieldset>");
 
             DirectoryInfo dirInfo = new DirectoryInfo(path);
             var allFiles = dirInfo.EnumerateFiles("*").OrderByDescending( x => x.CreationTime);
@@ -59,6 +67,7 @@ namespace HttpFileHandlerSample
 
         public void DeleteAllFiles(HttpContext context)
         {
+            //throw new Exception("Custom ExceptionCalled"); Test for Failture
             var path = context.Server.MapPath("~/upload");
             DirectoryInfo dirInfo = new DirectoryInfo(path);
             var allFiles = dirInfo.EnumerateFiles("*").OrderByDescending(x => x.CreationTime);
@@ -66,7 +75,6 @@ namespace HttpFileHandlerSample
             foreach (var file in allFiles)
             {
                 File.Delete(file.FullName);
-
             }
         }
 
