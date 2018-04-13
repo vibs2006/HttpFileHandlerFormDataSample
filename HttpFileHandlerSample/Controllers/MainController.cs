56043d1efc3bf5785260ef473f4bc9af098a359e
Vibs <vibs2006@gmail.com>
2018-04-13 09:30:13 +0530

    large file uploads

--- HttpFileHandlerSample/Controllers/MainController.cs
+++ HttpFileHandlerSample/Controllers/MainController.cs
@@ -14,8 +14,86 @@ namespace HttpFileHandlerSample.Controllers
 {
     public class MainController : ApiController
     {
+        [HttpGet]
+        public IHttpActionResult ok()
+        {
+            return Ok("vibs");
+        }
+
         [HttpPost]
-        public IHttpActionResult getAllDetails()
+        [ActionName("uploadLargeFormData")]
+        public async Task<IHttpActionResult> uploadLargeFormData()
+        {
+            
+            Stream rawStream =
+                HttpContext.Current.Request.GetBufferlessInputStream(true);
+
+
+            var streamContent = new StreamContent(rawStream);
+            foreach (var header in Request.Content.Headers)
+            {
+                streamContent.Headers.TryAddWithoutValidation(header.Key, header.Value);
+            }
+
+            var provider = new MultipartFormDataStreamProvider(HttpContext.Current.Server.MapPath("~/App_Data"));
+
+
+            var multipartFormContents = await streamContent.ReadAsMultipartAsync(provider);
+
+            var files = multipartFormContents.FileData;
+
+            //foreach (var file in files)
+            //{
+            //   files[]
+            //}
+
+
+            //for (int i = 0; i< multipartFormContents.Contents.Count; i++)
+            //{
+            //    var stream = await multipartFormContents.Contents[i].ReadAsStreamAsync();
+
+            //    var path = HttpContext.Current.Server.MapPath("~/upload");
+            //    if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+
+            //    FileStream fs = new FileStream(path + "/" + DateTime.Now.ToString("yyMMddHHmmss"), FileMode.CreateNew);
+                                
+            //        await Task.Delay(1000);
+            //        await stream.CopyToAsync(fs); 
+                
+
+                
+            //}
+            
+
+            //Stream reqStream = Request.Content.ReadAsStreamAsync().Result;
+            //MemoryStream tempStream = new MemoryStream();
+            //reqStream.CopyTo(tempStream);
+
+
+
+            //tempStream.Seek(0, SeekOrigin.End);
+            //StreamWriter writer = new StreamWriter(tempStream);
+            //writer.WriteLine();
+            //writer.Flush();
+            //tempStream.Position = 0;
+
+
+            //StreamContent streamContent = new StreamContent(tempStream);
+            //foreach (var header in Request.Content.Headers)
+            //{
+            //    streamContent.Headers.Add(header.Key, header.Value);
+            //}
+
+            //// Read the form data and return an async task.
+            //await streamContent.ReadAsMultipartAsync(provider);
+
+            
+
+            return Ok("Success");
+        }
+
+        //[HttpPost]
+        private async Task<IHttpActionResult> getAllDetails()
         {
             var isMultiForm = Request.Content.IsMimeMultipartContent();
 
@@ -35,7 +113,7 @@ namespace HttpFileHandlerSample.Controllers
                 // var multiProvider = provider.ReadAsMultipartAsync().Result;
                 //var multiStreamProvider = Request.Content.ReadAsMultipartAsync(provider).Result;
 
-                var objMultiPartMemoryStreamProvider = Request.Content.ReadAsMultipartAsync().Result;
+                var objMultiPartMemoryStreamProvider = await Request.Content.ReadAsMultipartAsync();
                 var multiPartContents = objMultiPartMemoryStreamProvider.Contents;
 
                 for (int i = 0; i < multiPartContents.Count; i++)
@@ -52,19 +130,31 @@ namespace HttpFileHandlerSample.Controllers
                     //If FileName is empty then it means its a form data collection
                     if (string.IsNullOrEmpty(contentDispositionHeaderDetails.FileName))
                     {
-                        var contentBody = httpContent.ReadAsStringAsync().Result;
+                        var contentBody = await httpContent.ReadAsStringAsync();
                         textWrite(contentBody);
                     }
                     else
                     {
-                        Stream fileStream = httpContent.ReadAsStreamAsync().Result;
-                        var uploadPath = HttpContext.Current.Server.MapPath("~/upload/");
-                        if (!Directory.Exists(uploadPath)) Directory.CreateDirectory(uploadPath);
+                        
 
-                        using (FileStream fs = new FileStream(uploadPath+DateTime.Now.ToString("yyyyMMddHHmmss"), FileMode.Append))
-                        {
-                            fileStream.CopyTo(fs);
-                        }
+                        //byte[] buffer = new byte[2048];
+                        //var buffer = await httpContent.ReadAsByteArrayAsync();
+
+                        //HttpContext.Current.Request.GetBufferlessInputStream()
+
+                        //var content = await new StreamContent(objMultiPartMemoryStreamProvider.Contents[i].ReadAsStreamAsync(),)
+
+                        // var stream = await httpContent.ReadAsStreamAsync()
+                        //Stream fileStream = await httpContent.ReadAsStreamAsync();
+                        //var uploadPath = HttpContext.Current.Server.MapPath("~/upload");
+                        //if (!Directory.Exists(uploadPath)) Directory.CreateDirectory(uploadPath);
+                        //var fileNameWithPath = "/" + uploadPath + DateTime.Now.ToString("yyyyMMddHHmmss");
+
+                        //File.WriteAllBytes(fileNameWithPath, buffer);
+                        //using (FileStream fs = new FileStream(uploadPath+DateTime.Now.ToString("yyyyMMddHHmmss"), FileMode.Append))
+                        //{
+                        //    await fileStream.CopyToAsync(fs);
+                        //}
 
 
                         //var file = File.Create(uploadPath + DateTime.Now.ToString("yyyyMMddHHmmss"));
